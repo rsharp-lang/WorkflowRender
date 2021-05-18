1372968eb6eb9ec7117fb7422650cb5e53714c84
guigang xie <gg.xie@bionovogene.com>
2021-05-18 14:00:57 +0800

    add dev notes

---
+++ R/graph.R
@@ -0,0 +1,9 @@
+
+#' Build graph from table content
+#' 
+#' @param table the edges list data in dataframe format.
+#' @param nodes the nodes property table
+#' 
+let buildGraph as function(table, nodes = NULL) {
+
+}
---
+++ R/table.R
@@ -0,0 +1,2 @@
+# tools for convert any dataframe as the required table format for build a network graph
+
