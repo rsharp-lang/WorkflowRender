45854babb17580841999c56840bb856e97d4eb95
xieguigang <xie.guigang@gcmodeller.org>
2023-03-22 22:55:54 +0800

    there is an executive context bug?

--- test/workspaceTest.R
+++ test/workspaceTest.R
@@ -2,11 +2,16 @@ require(WorkflowRender);
 
 init_context(@dir);
 app1 = app("test1", function(app, context) {
-    str(app);
-    str(context);
+    # str(app);
+    # str(context);
+    print("abc");
 });
 
 hook(app1);
+
+hook(app("echo", function(app, context) {
+    print(context$pipeline);
+}));
 
 print(workspace(app1));
 
