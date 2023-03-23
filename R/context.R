e962e3f68782fdbd199aca5a1de69f0fb8dc2ce8
guigang xie <xie.guigang@gcmodeller.org>
2023-03-23 10:34:14 +0800

    configs run workflow build

--- R/context.R
+++ R/context.R
@@ -17,9 +17,13 @@ const init_context = function(outputdir = "./") {
     set(globalenv(), "&[workflow_render]", context);
 }
 
+#' Mark end of the analysis workflow
+#' 
+#' @details release the resources of the pipeline workflow modules
+#' 
 const finalize = function() {
     print("workflow finalize, ");
-    print(`  Elapsed time '${time_span(now() - .get_context()$t0)}'`);
+    print(`*** Elapsed time '${time_span(now() - .get_context()$t0)}' ***`);
     sink();
 }
 
