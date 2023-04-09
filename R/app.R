b80e45f10d3a19bb90d91a160a9ea85cf4faa8fd
xieguigang <xie.guigang@gcmodeller.org>
2023-04-09 17:55:10 +0800

    set user parameters

--- R/app.R
+++ R/app.R
@@ -10,3 +10,25 @@ const app = function(name, analysis, desc = "no description") {
         call = analysis
     );
 }
+
+#' Set user parameters to the workflow context
+#' 
+#' @param configs a key-value paire tuple list object that
+#'    contains the workflow parameter values.
+#' 
+const set_config = function(configs) {
+    const ctx = .get_context();
+    const cfg = {
+        if (!["configs" in ctx]) {
+            ctx$configs = list();
+        }
+
+        ctx$configs;
+    }
+
+    for(name in names(configs)) {
+        cfg[[name]] = configs[[name]];
+    }
+
+    invisible(NULL);
+}
