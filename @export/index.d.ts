507280fcbed61f4379751cfea5159d922cf87b48
xieguigang <xie.guigang@gcmodeller.org>
2023-05-09 09:29:43 +0800

    Update index.d.ts

--- @export/index.d.ts
+++ @export/index.d.ts
@@ -3,20 +3,6 @@
 // package_source=WorkflowRender
 
 declare namespace WorkflowRender {
-   /**
-     * @param desc default value Is ``"no description"``.
-   */
-   function app(name:any, analysis:any, desc:string): object;
-   /**
-   */
-   function set_config(configs:any): object;
-   /**
-     * @param outputdir default value Is ``"./"``.
-   */
-   function init_context(outputdir:string): object;
-   /**
-   */
-   function finalize(): object;
    module _ {
       /**
       */
@@ -27,17 +13,31 @@ declare namespace WorkflowRender {
    }
    /**
    */
-   function hook(app:any): object;
+   function __runImpl(context:any): object;
+   /**
+     * @param desc default value Is ``"no description"``.
+   */
+   function app(name:any, analysis:any, desc:string): object;
    /**
    */
    function echo_warning(msg:any): object;
+   /**
+   */
+   function finalize(): object;
+   /**
+   */
+   function hook(app:any): object;
+   /**
+     * @param outputdir default value Is ``"./"``.
+   */
+   function init_context(outputdir:string): object;
    /**
      * @param registry default value Is ``NULL``.
    */
    function run(registry:any): object;
    /**
    */
-   function __runImpl(context:any): object;
+   function set_config(configs:any): object;
    /**
    */
    function workspace(app:any): object;
