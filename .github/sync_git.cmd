4043e1e19e0eaef6822a3d35eff0194e1ece107b
guigang xie <xie.guigang@gcmodeller.org>
2023-05-09 15:02:57 +0800

    Update sync_git.cmd

--- .github/sync_git.cmd
+++ .github/sync_git.cmd
@@ -1,3 +1,10 @@
 @echo off
 
 REM git remote add local http://git.biodeep.cn/xieguigang/WorkflowRender.git
+REM git remote add gitlink https://gitlink.org.cn/xieguigang/WorkflowRender.git
+
+git pull gitlink HEAD
+git pull local HEAD
+
+git push local HEAD
+git push gitlink HEAD
