54867fd11367dcd1c6636f4d60dc7f82901212bf
xieguigang <xie.guigang@gcmodeller.org>
2023-05-09 14:37:47 +0800

    Update hook.R

--- R/application/hook.R
+++ R/application/hook.R
@@ -5,7 +5,10 @@ const hook = function(app) {
     const pool = context$workflow;
 
     pool[[app$name]] = app;
+    # turn on/activate current analysis app by default
     context$pipeline = append(context$pool, app$name);
 
     set(globalenv(), "&[workflow_render]", context);
+
+    invisible(NULL);
 }
