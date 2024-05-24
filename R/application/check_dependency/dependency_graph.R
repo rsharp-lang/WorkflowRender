3b5c4f5d84e421c46e571d44a786cbcfe60a2e86
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-24 13:42:15 +0800

    how to create the graph?

--- R/application/check_dependency/dependency_graph.R
+++ R/application/check_dependency/dependency_graph.R
@@ -1,5 +1,19 @@
 #' try to extract the dependency graph from the application modules
 #' 
 const dependency_graph = function() {
+    let context = .get_context();
+    let appList = context$workflow;
+    let source      = [];   # name of source node
+    let source_type = [];  # type of the source node
+    let target      = [];   # name of the target node
+    let target_type = []; # type of the target node
+    let deps_type   = [];  # the dependency type
 
+    # start to analysis application module and generates the input and outputs
+
+}
+
+const dependency_analysis = function(app) {
+    str(app);
+    stop();
 }
