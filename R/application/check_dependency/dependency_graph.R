fd70319ade8b7f996d3f1f01b03c7d5e5f1bcd8f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-24 14:45:44 +0800

    make code style improvements

--- R/application/check_dependency/dependency_graph.R
+++ R/application/check_dependency/dependency_graph.R
@@ -55,7 +55,9 @@ const dependency_graph = function() {
         );
     }
 
-    
+    for(app in pool) {
+
+    }
 }
 
 const extract_workflow_vertex = function(meta) {
@@ -63,21 +65,34 @@ const extract_workflow_vertex = function(meta) {
     let i = 1;
     let files = meta$input_files;
 
-    vex[["app"]] = list(id = md5(`app_${meta$id}`), name = meta$id, label = meta$label, type = "application");
+    vex[["app"]] = list(id = md5(`app_${meta$id}`), 
+        name  = meta$id, 
+        label = meta$label, type = "application"
+    );
     
     for(name in meta$pars) {
-        vex[[`v_${i = i+1}`]] = list(id = md5(name), name = name, label = name, type = "environment");
+        vex[[`v_${i = i+1}`]] = list(id = md5(name), 
+            name = name, 
+            label = name, type = "environment");
     }
 
     for(app_name in names(files)) {
         let filenames = files[[app_name]];
 
         for(file in filenames) {
-            vex[[`f_${i=i+1}`]] = list(id = md5(`${app_name}://${file}`), name = file, label = `${app_name}://${file}`, type = "file");
+            vex[[`f_${i=i+1}`]] = list(id = md5(`${app_name}://${file}`), 
+                name = file, 
+                label = `${app_name}://${file}`, 
+                type = "file");
         }
     }
 
-    data.frame(id = vex@id, name = vex@name, label = vex@label, type = vex@type);
+    data.frame(
+        id = vex@id, 
+        name = vex@name, 
+        label = vex@label, 
+        type = vex@type
+    );
 }
 
 const dependency_analysis = function(app) {
