4ded54e328a54d0954eee5a97df3e864c9034c85
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-08-14 15:00:41 +0800

    try to parse the description help information from the comment docs

--- R/application/reflection.R
+++ R/application/reflection.R
@@ -15,6 +15,18 @@ const __build_app = function(f) {
     let context_env = attrs$context_env;
     let workfiles = attrs$workfiles;
     
+    if (str_empty(desc, test_empty_factor=TRUE)) {
+        # get description text from the function comment docs
+        let json_str = attr(f, which = "r-sharp-help");
+
+        if (!is.null(json_str)) {
+            json_str = JSON::json_decode(json_str);
+            json_str = (json_str$description) || (json_str$title);
+
+            desc <- json_str; 
+        }
+    }
+
     if (nchar(app_name) == 0) {
         echo_warning([
             "invalid application function configuration was found:", 
